3b4fd8aef8d606ffa8d3b4e68110769bab64fe01
Diogoaasousa23 <diogoaadesousa@gmail.com>
2024-06-06 10:41:16 +0100

    submit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 
-const router = require('./routes/index');
+const routerLocal = require('./routes/local/index');
+const routerPgs = require('./routes/pgs/index');
 const publicoRouter = require('./routes/publico');
 const privadoRouter = require('./routes/privado');
 
@@ -12,11 +13,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', publicoRouter);
+//app.use('/', publicoRouter);
 app.use('/bo/', privadoRouter);
-app.use('/api/', router);
+app.use('/api/local/', routerLocal);
+app.use('/api/pgs/', routerPgs);
 
-const port = process.env.SERVER_PORT || 8080;
+const port = process.env.SERVER_PORT || 4242;
 app.listen(port, () => {
     console.log('Express server listening on port', port)
 });
